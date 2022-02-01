0bd7ec24505ddccc4b9f24094c1e947fb25cfd63
Hardik Sharma <hardik00051@gmail.com>
2022-02-01 21:39:57 +0530

    Added angular folder

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const url = require('url');
 const path = require('path');
 
+if (require('electron-squirrel-startup')) return app.quit();
 
 let mainWindow;
 
